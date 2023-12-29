61057827d61d64bf198436db4867e134f8897222
huhuiling <1378576508@qq.com>
2023-12-29 12:32:23 +0800

    基础页面搭建

---
+++ plop-demo/my-vue-project/src/views/page/erg.vue
@@ -0,0 +1,17 @@
+<script setup name="PageErg">
+// const router = useRouter()
+// const route = useRoute()
+</script>
+
+<template>
+    <div>
+        <page-header title="er" />
+        <page-main>
+            页面内容区域
+        </page-main>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+// scss
+</style>
